9896e4ae13b3edcfe0222a7fdd0ad4ec215c4c6c
jyhc <jyhc40@hotmail.com>
2017-03-05 18:21:08 -0800

    test2

--- Percolation/src/PercolationStats.java
+++ Percolation/src/PercolationStats.java
@@ -23,16 +23,33 @@ public class PercolationStats {
 	public double stddev(){  // sample standard deviation of percolation threshold
 		return StdStats.stddev(sitefraction);
 	}
-	public double confidenceLo(){   // low  endpoint of 95% confidence interval
-		
-	}
-	public double confidenceHi(){  	// high endpoint of 95% confidence interval
-		
-	}
+//	public double confidenceLo(){   // low  endpoint of 95% confidence interval
+//		
+//	}
+//	public double confidenceHi(){  	// high endpoint of 95% confidence interval
+//		
+//	}
 	   
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		StdRandom.uniform(a, b)
+	public static void main(String[] args) {	  
+	    int n=Integer.parseInt(args[0]);
+	    int trials=Integer.parseInt(args[1]);
+	    PercolationStats stats= new PercolationStats(n,trials);
+	    
+        for(int j = 0; j < trials/*perc.length*/; j++){
+            int count=0;
+            while(stats.perc[j].percolates()!=true){
+                int row=StdRandom.uniform(1, n+1);
+                int col=StdRandom.uniform(1, n+1);
+                if(stats.perc[j].isOpen(row, col)==false){
+                    stats.perc[j].open(row, col);
+                    count++;
+                }
+                
+            }
+            System.out.print(count+"\n");
+            System.out.print(stats.perc[j].numberOfOpenSites()+"\n");
+        }     
+	    
 	}
 
 }
