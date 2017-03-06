1a6de8018f06362bd622ff0dbc188d66cba1b054
jyhc <jyhc40@hotmail.com>
2017-03-05 19:32:13 -0800

    working version

--- Percolation/src/PercolationStats.java
+++ Percolation/src/PercolationStats.java
@@ -5,6 +5,7 @@ public class PercolationStats {
 
 	private Percolation[] perc;
 	private double sitefraction[];
+	private double times;
 	
 	public PercolationStats(int n, int trials){    // perform trials independent experiments on an n-by-n grid
 		if (n<=0 || trials <=0){
@@ -15,6 +16,7 @@ public class PercolationStats {
 		for(int i = 0; i < trials/*perc.length*/; i++){
 			perc[i] = new Percolation(n);
 		}
+		times=trials;
 	}
 		
 	public double mean(){ // sample mean of percolation threshold
@@ -23,33 +25,32 @@ public class PercolationStats {
 	public double stddev(){  // sample standard deviation of percolation threshold
 		return StdStats.stddev(sitefraction);
 	}
-//	public double confidenceLo(){   // low  endpoint of 95% confidence interval
-//		
-//	}
-//	public double confidenceHi(){  	// high endpoint of 95% confidence interval
-//		
-//	}
+	public double confidenceLo(){   // low  endpoint of 95% confidence interval
+		return mean()-1.96*stddev()/Math.sqrt(times);
+	}
+	public double confidenceHi(){  	// high endpoint of 95% confidence interval
+	    return mean()+1.96*stddev()/Math.sqrt(times);
+	}
 	   
 	public static void main(String[] args) {	  
 	    int n=Integer.parseInt(args[0]);
 	    int trials=Integer.parseInt(args[1]);
 	    PercolationStats stats= new PercolationStats(n,trials);
 	    
-        for(int j = 0; j < trials/*perc.length*/; j++){
-            int count=0;
+        for(int j = 0; j < trials/*perc.length*/; j++){            
             while(stats.perc[j].percolates()!=true){
                 int row=StdRandom.uniform(1, n+1);
                 int col=StdRandom.uniform(1, n+1);
                 if(stats.perc[j].isOpen(row, col)==false){
-                    stats.perc[j].open(row, col);
-                    count++;
+                    stats.perc[j].open(row, col);                    
                 }
                 
             }
-            System.out.print(count+"\n");
-            System.out.print(stats.perc[j].numberOfOpenSites()+"\n");
+            stats.sitefraction[j]=(double)stats.perc[j].numberOfOpenSites()/(n*n);
+            
         }     
-	    
+        System.out.print("mean\t"+stats.mean()+"\nstddev\t"+stats.stddev());
+        System.out.print("\n95% confidence interval\t["+stats.confidenceLo()+", "+stats.confidenceHi()+"]\n");
 	}
 
 }
